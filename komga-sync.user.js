4f3382ddf3a5806226f5f721eaefc586f3936efa
lmaonator <lmaonator@gmail.com>
2024-01-16 12:24:42 +0100

    fix: var is evil

--- komga-sync.user.js
+++ komga-sync.user.js
@@ -654,8 +654,8 @@
     function randStr(length) {
         const chars =
             "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        var result = "";
-        for (var i = length; i > 0; --i) {
+        let result = "";
+        for (let i = length; i > 0; --i) {
             result += chars[Math.floor(Math.random() * chars.length)];
         }
         return result;
